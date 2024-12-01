0cef3b6a37ed732159f380164b30805beb58ceed
imayanknegi29 <imayanknegi29@gmail.com>
2024-12-01 20:13:02 +0530

    TODO app

---
+++ ios-coredata/Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mohammad Azam on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
